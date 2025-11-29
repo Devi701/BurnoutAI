3a1cee98ac26555f17dc1e4f269248ee8cd9b7e2
Pardeep Kumar Maheshwari <pardeepkumarmaheshwari@Pardeeps-iMac.local>
2025-11-29 17:15:34 +0000

    Updated google analytics

--- BurnoutApp.py
+++ BurnoutApp.py
@@ -11,12 +11,13 @@ import matplotlib.pyplot as plt
 # GOOGLE ANALYTICS (load BEFORE page_config)
 # ========================
 st.markdown("""
-<!-- Google Analytics -->
+<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-1YTTPBS985"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
+
   gtag('config', 'G-1YTTPBS985');
 </script>
 """, unsafe_allow_html=True)
